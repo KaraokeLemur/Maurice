496900cb56a204e62a345417e58ac744e64b2069
KaraokeLemur <markku.eerola@gmail.com>
2016-01-20 18:58:55 +0100

    Initial commit

---
+++ MauricePOS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MauricePOS.Startup))]
+namespace MauricePOS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
